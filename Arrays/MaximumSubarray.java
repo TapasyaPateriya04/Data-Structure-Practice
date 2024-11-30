e17d998f8b963be0d03d395567f4212dbaf6db73
Tapasya Pateriya <tapasyapateriya04@gmail.com>
2024-11-30 09:31:00 +0530

    optimised code

--- Arrays/MaximumSubarray.java
+++ Arrays/MaximumSubarray.java
@@ -1,18 +1,37 @@
 import java.util.*;
 public class MaximumSubarray {
-    public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
+    // public int maxSubArray(int[] nums) {
+    //     int max = Integer.MIN_VALUE;
+    //     int sum = 0;
         
+    //     for (int i = 0; i < nums.length; i++) {            O(n^2)
+    //                                                       Brute Force
+    //         sum = 0;
+    //         for (int j = i; j < nums.length; j++) {
+    //             sum += nums[j];
+    //             max = Math.max(max, sum);
+    //         }
+    //     }
+    //     return max;
+    // }
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE; 
+        int sum = 0;                
         for (int i = 0; i < nums.length; i++) {
-            sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                max = Math.max(max, sum);
+            sum += nums[i];           
+      //                                                     O(n)
+      //                                                    optimised
+            if (sum > max) {        
+                max = sum;
+            }
+            
+            if (sum < 0) {        
+                sum = 0;
             }
         }
         return max;
     }
+
 
     public static void main(String[] args) {
         MaximumSubarray solution = new MaximumSubarray();
