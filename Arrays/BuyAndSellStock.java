beb6326211ddc37981fa45c2da163152c2351b08
Tapasya Pateriya <tapasyapateriya04@gmail.com>
2024-11-30 10:20:36 +0530

    optimised code

--- Arrays/BuyAndSellStock.java
+++ Arrays/BuyAndSellStock.java
@@ -1,12 +1,28 @@
 public class BuyAndSellStock {
-    public int maxProfit(int[] prices) {
-        int maxProfit = Integer.MIN_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
-            }                                                                      
+
+    // public int maxProfit(int[] prices) {
+    //     int maxProfit = Integer.MIN_VALUE;
+    //     for (int i = 0; i < prices.length; i++) {
+    //         for (int j = i + 1; j < prices.length; j++) {                        //     O(n^2)
+    //                                                                             //    Brute Force
+    //             maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+    //         }                                                                      
+    //     }
+    //     return maxProfit > 0 ? maxProfit : 0;
+    // }
+
+public int maxProfit(int[] prices) {
+        
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+                                                       //          O(n)
+        for (int i = 0; i < prices.length; i++) {     //        Optimised
+
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
         }
-        return maxProfit > 0 ? maxProfit : 0;
+
+        return maxProfit;
     }
 
     public static void main(String[] args) {
